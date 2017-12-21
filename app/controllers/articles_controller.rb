df1f98c61dd11b6e5c66b4f246fc51be8f6e168b
duangduangbo <575620657@qq.com>
2017-12-21 10:02:54 +0800

    rails 入门完成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "123456", password: "123456", except: [:index, :show]
     def edit
         @article=Article.find(params[:id])
     end
