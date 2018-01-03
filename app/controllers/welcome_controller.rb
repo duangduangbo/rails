efeb8697352f8d4af77752044db443c57ab02667
duangduangbo <575620657@qq.com>
2018-01-03 09:40:05 +0800

    表单

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,24 @@
 class WelcomeController < ApplicationController
   def index
     @articles=Article.all
+    # @comment=Comment.new(article_id:2)
+    
     # render inline: "<% @articles.each do |p| %><p><%= p.text %></p><% end %>"
     # @json="123"
     # render json: @json
    
   end
    
+def upload
+  uploaded_io = params[:articles][:text]
+  File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+    file.write(uploaded_io.read)
+  end
+end
 
+end
+class LabellingFormBuilder < ActionView::Helpers::FormBuilder
+  def text_field(attribute, options={})
+  label(attribute) 
+  end
 end
