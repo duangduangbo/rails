a47b58dccfef38450829726c3e27dda12a96e3e2
duangduangbo <575620657@qq.com>
2018-01-03 15:56:09 +0800

    访问和删除session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,31 @@
 class UsersController < ApplicationController
   
     def new
-        
+        @user=User.new
     end
   def create
-    @user=User.new(params_user)
-    if @user.save
-        redirect_to root_url
-    else render 'register'
-    end
+    @user=User.new(params_user) 
+     @user.save   
+     session[:current_user_id]=@user.id
+    flash[:notice]="注册成功"
+    redirect_to :root
   end
-  
+
     def login
     end
     def register
-        @user=User.new
+        user=User.find_by_name(params[:name])
+        if user&&user.authenticate(params[:name],params[:password])
+            session[:current_user_id]=user.id
+             redirect_to :root 
+         else 
+             render 'new'
+         end
     end
-
+    def logout
+        @_current_user=session[:current_user_id]=nil
+        redirect_to :root
+    end
     private
 
     def params_user
